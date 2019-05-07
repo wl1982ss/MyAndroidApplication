03e4f95156e2846d9962d9413f2e4a0073684532
wl1982ss <32494514@qq.com>
2019-05-07 18:50:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,200 @@
+package com.example.myapplication;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
+    private WebView webView;
+
+    private TextView show_message;
+    private TextView responseText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+//        Button button_pic = (Button) findViewById(R.id.button_pic);
+//        Button button_progress = (Button) findViewById(R.id.button_progress);
+        Button button_dialog = (Button) findViewById(R.id.button_dialog);
+//        Button button_progress_dialog = (Button) findViewById(R.id.button_progress_dialog);
+//        Button button_web_view = (Button) findViewById(R.id.button_web_view);
+        Button button_send_request = (Button) findViewById(R.id.button_send_request);
+
+        show_message = (TextView) findViewById(R.id.text_view);
+        editText = (EditText) findViewById(R.id.edit_text);
+//        imageView = (ImageView) findViewById(R.id.image_view);
+//        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+//        webView = (WebView) findViewById(R.id.web_view);
+        responseText = (TextView) findViewById(R.id.response_text);
+//        button.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                // 在此处添加逻辑
+//                Toast.makeText(MainActivity.this,"加载失败",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        button.setOnClickListener(this);
+//        button_pic.setOnClickListener(this);
+//        button_progress.setOnClickListener(this);
+        button_dialog.setOnClickListener(this);
+//        button_progress_dialog.setOnClickListener(this);
+//        button_web_view.setOnClickListener(this);
+        button_send_request.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.button:
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                break;
+//            case R.id.button_pic:
+////                imageView.setImageResource(R.drawable.smile);
+//                break;
+//            case R.id.button_progress:
+//                if(progressBar.getVisibility() == View.GONE) {
+//                    progressBar.setVisibility(View.VISIBLE);
+//                    int progress = progressBar.getProgress();
+//                    progress = progress + 10;
+//                    progressBar.setProgress(progress);
+//                } else {
+//                    progressBar.setVisibility(View.GONE);
+//                }
+//                break;
+            case R.id.button_dialog:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("This is Dialog");
+                dialog.setMessage("Something important.");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.show();
+                break;
+//            case R.id.button_progress_dialog:
+//                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+//                progressDialog.setTitle("This is ProgressDialog");
+//                progressDialog.setMessage("Loading...");
+//                progressDialog.setCancelable(true);
+//                progressDialog.show();
+//                break;
+//            case R.id.button_web_view:
+//                webView.getSettings().setJavaScriptEnabled(true);
+//                webView.setWebViewClient(new WebViewClient());
+//                webView.loadUrl("http://www.baidu.com");
+//                break;
+            case R.id.button_send_request:
+                sendRequestWithHttpURLConnection();
+                //sendRequestWithOkHttp();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void sendRequestWithHttpURLConnection(){
+        // 开启线程来发起网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try{
+                    URL url = new URL("http://192.168.1.110/SmartConnect/public/index/show_message");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+//                    connection.setConnectTimeout(8000);
+//                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    // 下面对获取到的输入流进行读取
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while((line = reader.readLine()) != null){
+                        response.append(line);
+                    }
+                    showResponse(response.toString());
+                } catch (Exception e){
+                    e.printStackTrace();
+                } finally {
+                    if(reader != null){
+                        try{
+                            reader.close();
+                        } catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    if(connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void showResponse(final String response) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // 在这里进行UI操作，将结果显示到界面上
+                //responseText.setText(response);
+                show_message.setText(response);
+            }
+        });
+    }
+
+    private void sendRequestWithOkHttp() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder().url("http://www.baidu.com").build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    showResponse(responseData);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
